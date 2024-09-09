a62270004cf39615e936ccbcc052c4c23b70504b
Vashist213-lab <vashist213@hotmail.com>
2024-09-09 14:07:16 +0200

    Initial commit

--- webroot/index.php
+++ webroot/index.php
@@ -1,63 +1,67 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>LAMP STACK</title>
-        <link rel="stylesheet" href="/assets/css/bulma.min.css">
-    </head>
-    <body>
-        <section class="hero is-medium is-info is-bold">
-            <div class="hero-body">
-                <div class="container has-text-centered">
-                    <h1 class="title">
-                        LAMP STACK [DEV]
-                    </h1>
-                    <h2 class="subtitle">
-                        Your local development environment
-                    </h2>
-                </div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LAMP STACK</title>
+    <link rel="stylesheet" href="/assets/css/bulma.min.css">
+</head>
+<body>
+    <section class="hero is-medium is-info is-bold">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <h1 class="title">
+                    LAMP STACK [DEV]
+                </h1>
+                <h2 class="subtitle">
+                    Your local development environment
+                </h2>
             </div>
-        </section>
-        <section class="section">
-            <div class="container">
-                <div class="columns">
-                    <div class="column">
-                        <h3 class="title is-3 has-text-centered">Environment</h3>
-                        <hr>
-                        <div class="content">
-                            <ul>
-                                <li><?= apache_get_version(); ?></li>
-                                <li>PHP <?= phpversion(); ?></li>
-                                <li>
-                                    <?php
-                                    $link = mysqli_connect("mysql", "root", "tiger", null);
+        </div>
+    </section>
+    <section class="section">
+        <div class="container">
+            <div class="columns">
+                <div class="column">
+                    <h3 class="title is-3 has-text-centered">Environment</h3>
+                    <hr>
+                    <div class="content">
+                        <ul>
+                            <li><?= apache_get_version(); ?></li>
+                            <li>PHP <?= phpversion(); ?></li>
+                            <li>
+                                <?php
+                                $servername = getenv('DB_SERVER') ?: 'mysql';
+                                $username = 'root';
+                                $password = getenv('MYSQL_ROOT_PASSWORD') ?: 'rootpassword';
 
-                                    /* check connection */
-                                    if (mysqli_connect_errno()) {
-                                        printf("MySQL connecttion failed: %s", mysqli_connect_error());
-                                    } else {
-                                        /* print server version */
-                                        printf("MySQL Server %s", mysqli_get_server_info($link));
-                                    }
-                                    /* close connection */
-                                    mysqli_close($link);
-                                    ?>
-                                </li>
-                            </ul>
-                        </div>
+                                $link = mysqli_connect($servername, $username, $password);
+
+                                /* check connection */
+                                if (mysqli_connect_error()) {
+                                    printf("MySQL connection failed: %s", mysqli_connect_error());
+                                } else {
+                                    /* print server version */
+                                    printf("MySQL Server %s", mysqli_get_server_info($link));
+                                }
+                                /* close connection */
+                                mysqli_close($link);
+                                ?>
+                            </li>
+                        </ul>
                     </div>
-                    <div class="column">
-                        <h3 class="title is-3 has-text-centered">Quick Links</h3>
-                        <hr>
-                        <div class="content">
-                            <ul>
-                                <li><a href="/phpinfo.php">phpinfo()</a></li>
-                            </ul>
-                        </div>
+                </div>
+                <div class="column">
+                    <h3 class="title is-3 has-text-centered">Quick Links</h3>
+                    <hr>
+                    <div class="content">
+                        <ul>
+                            <li><a href="/phpinfo.php">phpinfo()</a></li>
+                        </ul>
                     </div>
                 </div>
             </div>
-        </section>
-    </body>
+        </div>
+    </section>
+</body>
 </html>
